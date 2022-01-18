cc889affa2be87422f0cbea7bbff2a52b5a9d467
krendeleno <krendeleno@gmail.com>
2022-01-18 18:46:25 +0800

    feat: add autocomplete

--- src/components/Main/MainInput/index.js
+++ src/components/Main/MainInput/index.js
@@ -2,6 +2,8 @@ import './style.css';
 import {useContext} from "react";
 import {Context} from "../../Context";
 import {useNavigate} from "react-router-dom";
+import Autocomplete from "react-autocomplete"
+import {services} from "../../../database/services";
 
 function MainInput() {
     let navigate = useNavigate();
@@ -31,7 +33,20 @@ function MainInput() {
             </div>
             <div className="form-container">
                 <form onSubmit={handleSubmit}>
-                    <input value={context.search} onChange={handleChange} className="main-input" type="text" placeholder="найдется каждый сервис"/>
+                    <Autocomplete
+                        className="main-input"
+                        getItemValue={(item) => item.label}
+                        items={services.map(x => ({id: x.id, label: x.name.toLowerCase()}))}
+                        renderItem={(item, isHighlighted) =>
+                            <div className="autocomplete" key={item.id} style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+                                {item.label}
+                            </div>
+                        }
+                        inputProps={{ placeholder: 'Найдется каждый сервис' }}
+                        value={context.search}
+                        onChange={handleChange}
+                        onSelect={(val) => {setContext(values => ({...values, search: val}));  navigate("/search");}}
+                    />
                     <input className="search-button" type="submit" value="Найти" />
                 </form>
 
